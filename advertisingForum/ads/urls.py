6d66fed77b7fad081f04209f20061ddbd494b639
beata <operchalska.beata@gmail.com>
2022-05-20 20:22:35 +0200

    Log in fixed

--- advertisingForum/ads/urls.py
+++ advertisingForum/ads/urls.py
@@ -6,6 +6,7 @@ app_name = 'ads'
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('register', views.register, name = 'register'),
-    path('login', views.log, name = 'log'),
-    path('logout', views.logoutuser, name = 'logout')
+    path('log', views.log, name = 'log'),
+    path('logout', views.logoutuser, name = 'logout'),
+    path('create', views.create, name = 'create'),
 ]
--- advertisingForum/ads/views.py
+++ advertisingForum/ads/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
+from .forms import AdForm
 
 # Create your views here.
 
@@ -15,8 +17,8 @@ def register(request):
     else:
         if request.POST.get('password1') == request.POST.get('password2'):
             try:
-                user = User.objects.create_user(username = request.POST.get('username'), password = request.POST.get('password'))
-            except:
+                user = User.objects.create_user(username = request.POST.get('username'), password = request.POST.get('password1'))
+            except IntegrityError:
                 error = 'This username is already taken. Try using different one'
                 return render(request, 'register.html', {'form' : UserCreationForm(), 'error' : error})
             else:
@@ -30,14 +32,17 @@ def log(request):
     if request.method == "GET":
         return render(request, 'log.html', {'form' : AuthenticationForm()})
     else:
-        user = authenticate(username = request.POST.get('username', password = request.POST.get('password')))
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
         if user is not None:
             login(request,user)
-            return redirect('home')
+            return redirect('ads:home')
         else:
             error = 'Username or password is wrong. Try again'
             return render(request, 'log.html', {'form' : AuthenticationForm(), 'error' : error})
 
 def logoutuser(request):
      logout(request)
-     return render(request, 'home')
+     return render(request, 'home.html')
+
+def create(request):
+    return render(request, 'create.html', {'form' : AdForm()})
